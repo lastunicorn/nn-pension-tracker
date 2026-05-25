09c208d7e9b983d04cbd7099d89757fafa07bcca
Alexandru Iuga <alex.iuga@gmail.com>
2026-05-25 19:36:42 +0300

    Delete unused class ColumnNamesRepository

--- sources/NnPensionTracker.Ports.DataAccess/ColumnNamesRepository.cs
+++
@@ -1,7 +0,0 @@
-namespace DustInTheWind.NnPensionTracker.Ports.DataAccess;
-
-internal static class ColumnNamesRepository
-{
-    private const string DatabasePath = "Data";
-    private const string FileName = "column-names.json";
-}
