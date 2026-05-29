260ad44a95326e624d79cf2713a221d0dd6b78d8
Alexandru Iuga <alex.iuga@gmail.com>
2026-05-29 14:43:57 +0300

    Fixed app directory name for linux

--- sources/NnPensionTracker.Cli/DeploymentEnvironment.cs
+++ sources/NnPensionTracker.Cli/DeploymentEnvironment.cs
@@ -2,6 +2,7 @@ namespace DustInTheWind.NnPensionTracker.Cli;
 
 internal class DeploymentEnvironment
 {
+    private const string DatabaseDirectoryName = "Data";
     private readonly bool isPortable;
     private readonly string appDirectoryName;
 
@@ -11,21 +12,32 @@ internal class DeploymentEnvironment
 
     public DeploymentEnvironment()
     {
-        string portableMarkerFileName = "portable";
-        string portableMarkerFilePath = Path.Combine(AppContext.BaseDirectory, portableMarkerFileName);
-        isPortable = File.Exists(portableMarkerFilePath);
-
-        if (isPortable)
-            appDirectoryName = string.Empty;
-        else if (OperatingSystem.IsWindows())
-            appDirectoryName = "Nn Pension Tracker";
-        else if (OperatingSystem.IsLinux())
-            appDirectoryName = ".nn-pension-tracker";
-        else
-            appDirectoryName = string.Empty;
+        isPortable = DetectIsPortable();
+        appDirectoryName = ResolveAppDirectoryName();
 
         AppSettingsFilePaths = ResolveAppSettingsFilePaths();
         DataDirectoryPath = ResolveDataDirectoryPath();
+    }
+
+    private bool DetectIsPortable()
+    {
+        string portableMarkerFileName = "portable";
+        string portableMarkerFilePath = Path.Combine(AppContext.BaseDirectory, portableMarkerFileName);
+        return File.Exists(portableMarkerFilePath);
+    }
+
+    private string ResolveAppDirectoryName()
+    {
+        if (isPortable)
+            return string.Empty;
+
+        if (OperatingSystem.IsWindows())
+            return "Nn Pension Tracker";
+
+        if (OperatingSystem.IsLinux())
+            return "nn-pension-tracker";
+
+        return string.Empty;
     }
 
     private IEnumerable<string> ResolveAppSettingsFilePaths()
@@ -58,20 +70,20 @@ internal class DeploymentEnvironment
     private string ResolveDataDirectoryPath()
     {
         if (isPortable)
-            return Path.Combine(AppContext.BaseDirectory, "Data");
+            return Path.Combine(AppContext.BaseDirectory, DatabaseDirectoryName);
 
         if (OperatingSystem.IsWindows())
         {
             string userAppData = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-            return Path.Combine(userAppData, appDirectoryName, "Data");
+            return Path.Combine(userAppData, appDirectoryName, DatabaseDirectoryName);
         }
 
         if (OperatingSystem.IsLinux())
         {
             string userHome = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-            return Path.Combine(userHome, appDirectoryName, "Data");
+            return Path.Combine(userHome, appDirectoryName, DatabaseDirectoryName);
         }
 
-        return Path.Combine(AppContext.BaseDirectory, "Data");
+        return Path.Combine(AppContext.BaseDirectory, DatabaseDirectoryName);
     }
 }
